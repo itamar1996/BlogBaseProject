f80a84c276becbf89910b50c279a4e144a0ce4ac
itamar1996 <itay9919@gmail.com>
2024-10-10 10:07:14 +0300

    create pot ant comment schema

--- src/models/postModel.ts
+++ src/models/postModel.ts
@@ -7,14 +7,46 @@ export interface IComment {
 }
 
 export interface IPost extends Document {
-  _id: Types.ObjectId;
   title: string;
   content: string;
   author: Types.ObjectId;
   comments: IComment[];
 }
 
-const CommentSchema = new Schema<IComment>({});
+
+const CommentSchema = new mongoose.Schema<IComment>({
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User' // מוודא שהשדה מקושר למודל של משתמש
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now // הוספת ערך ברירת מחדל לתאריך
+  },
+});
+const PostSchema = new mongoose.Schema<IPost>({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  comments: [CommentSchema], // הוספת תגובות ישירות באמצעות הסכמה של תגובות
+});
 
 
-export default mongoose.model<IPost>("Post");
+const Comment = mongoose.model('Comment', CommentSchema);
+const Post = mongoose.model('Post', PostSchema);
